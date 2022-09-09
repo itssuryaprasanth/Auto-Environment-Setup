98cc330956b19bc58a5c095317622555482d17d9
Surya Prasanth <69471952+TestingAPI72@users.noreply.github.com>
2022-09-09 17:41:46 +0530

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -5,6 +5,6 @@ set MyServer=www.google.com
 %SystemRoot%\system32\ping.exe -n 1 %MyServer% >nul
 if errorlevel 1 goto NoServer
 echo %MyServer% is available.
-Powershell.exe -File Files_download_installer/Script_Installer.ps1
+Powershell.exe -File Script_Installer.ps1
 cmd /c pip install -r requirements.txt
 pause
